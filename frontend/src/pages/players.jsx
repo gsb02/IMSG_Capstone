58db43c11ed0715b8613ee507a38c1e8cae570cb
Graham Bridges <graham.bridges@maine.edu>
2024-02-16 14:03:53 -0500

    Added css styling for players table

--- frontend/src/pages/players.jsx
+++ frontend/src/pages/players.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import './players.css';
 
+
 const Players = () => {
     const [players, setPlayers] = useState([]);
 
@@ -22,15 +23,43 @@ const Players = () => {
     }, []);
 
     return (
-        <div>
-            <h1>Player List</h1>
-            <ul>
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+            <h1 style={{textAlign: "center"}}>Player List</h1>
+            <table style = {{ textAlign: "center",  backgroundColor: "white", borderStyle: "solid", margin: "5px", borderCollapse: "collapse"}}>
+                <tr>
+                    <th scope = "col">
+                        Name
+                    </th>
+                    <th scope = "col">
+                        Age
+                    </th>
+                    <th scope = "col">
+                         Class
+                    </th>
+                </tr>
                 {players.map((player, index) => (
-                    <li key={index}>
+                     /*<li style = {{textAlign: "center", backgroundColor: "white", }} key={index}>
                         Name: {player.playerName}, Age: {player.age}, Class: {player.class}
-                    </li>
+                    </li>*/
+                    <tr>
+                        <td style={{ borderBottom: "1px solid black" }}>
+                            {player.playerName}
+                        </td>
+                        <td style={{ borderBottom: "1px solid black" }}>
+                            {player.age}
+                        </td>
+                        <td style={{ borderBottom: "1px solid black" }}>
+                            {player.class}
+                        </td>
+                        <td  style={{ borderBottom: "1px solid black" }}>
+                            <button>Edit</button>
+                        </td>
+                        <td  style={{ borderBottom: "1px solid black" }}>
+                            <button>Delete</button>
+                        </td>
+                    </tr>
                 ))}
-            </ul>
+            </table>
         </div>
     );
 };
