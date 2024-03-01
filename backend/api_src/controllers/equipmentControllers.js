fdb009edc0666c1b06397af286421ce26a14b526
brennanpoitras <112443840+brennanpoitras@users.noreply.github.com>
2024-03-01 12:17:37 -0500

    feats: get equipment and sub attributes

--- backend/api_src/controllers/equipmentControllers.js
+++ backend/api_src/controllers/equipmentControllers.js
@@ -2,34 +2,37 @@ import Equipment from "../models/Equipment.js";
 import Apparel from "../models/Apparel.js";
 
 export const createEquipment = async (req, res, next) => {
-    let { equipmentName, storedQuantity, distQuantity, sportID, equipmentType, lastOrdered, lastDistributed, attributes } = req.body;
-    
-    
-    let equipment = new Equipment(null, equipmentName, storedQuantity, distQuantity, sportID, equipmentType, lastOrdered, lastDistributed);
-    equipment = await equipment.createEquipment();
-    console.log(equipment);
 
-    
-    let equipmentID = await Equipment.getMostRecentInsertID();
-    
-    //check which equipment to create
-    switch (equipmentType) {
+    try{
+        let { equipmentName, storedQuantity, distQuantity, sportID, equipmentType, lastOrdered, lastDistributed, attributes } = req.body;
+        
+        let equipment = new Equipment(null, equipmentName, storedQuantity, distQuantity, sportID, equipmentType, lastOrdered, lastDistributed);
+        equipment = await equipment.createEquipment();
+        console.log(equipment);
 
-        //apparel
-        case 1:
-            
-            let { brandName, qSmall, qMed, qLarge, qXL, qXXL, q3X } = attributes;
+        let equipmentID = await Equipment.getMostRecentInsertID();
+        
+        //check which equipment to create
+        switch (equipmentType) {
 
-            let apparel = new Apparel(null, brandName, equipmentID, qSmall, qMed, qLarge, qXL, qXXL, q3X);
-            apparel = await apparel.createApparel();
-            console.log(apparel);
-            break;
+            //apparel
+            case 1:
+                
+                let { brandName, qSmall, qMed, qLarge, qXL, qXXL, q3X } = attributes;
 
-        //accessories
-        case 2:
-            break;
+                let apparel = new Apparel(null, brandName, equipmentID, qSmall, qMed, qLarge, qXL, qXXL, q3X);
+                apparel = await apparel.createApparel();
+                console.log(apparel);
+                break;
+
+            //accessories
+            case 2:
+                break;
+        }
+    } catch(error){
+        res.status(500).json({error: "error creating equipment"})
     }
-    res.send("create equipment route");
+    
 }
 
 export const deleteEquipmentByID = async (req, res, next) => {
@@ -45,4 +48,76 @@ export const deleteEquipmentByID = async (req, res, next) => {
         let apparel = await Apparel.deleteApparelByEquipmentID(equipmentID);
     }
     res.status(200).json(equipment);
+}
+
+export const getAllEquipment = async (req, res, next) => {
+
+    try{
+
+        let [equipment, _] = await Equipment.getAllEquipment();
+        res.status(200).json(equipment);  
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: "failed to get all equipment"}); 
+    }
+    
+}
+
+export const getAllEquipmentByType = async (req, res, next) => {
+
+    try{
+
+        let equipmentType = req.params.equipmentType;
+        let [equipment, _] = await Equipment.getAllEquipmentByType(equipmentType);
+        res.status(200).json(equipment);
+
+    } catch (error){
+        console.log(error);
+        res.status(500).json({error: "failed to get all equipment by type"});
+    }
+
+}
+
+export const getEquipmentPrimaryAttributesByID = async (req, res, next) => {
+
+    try{
+
+        let equipmentID = req.params.equipmentID;
+        let [equipment, _] = await Equipment.getEquipmentPrimaryAttributesByID(equipmentID);
+        res.status(200).json(equipment);
+
+    } catch(error){
+        console.log(error);
+        res.status(500).json({error: "failed to get equipment attributes"});
+    }
+}
+
+export const getEquipmentSecondaryAttributesByID = async (req, res, next) => {
+
+    try{
+
+        let equipmentID = req.params.equipmentID;
+        let equipmentType = await Equipment.getEquipmentTypeByID(equipmentID);
+        
+        switch (equipmentType){
+            
+            //apparel
+            case 1:
+                let subTable = "apparel";
+                let [apparel, _] = await Equipment.getEquipmentSecondaryAttributesByID(subTable, equipmentID);
+                res.status(200).json(apparel);
+                break;
+
+            default:
+                break;
+        }
+
+        
+
+    } catch(error){
+        console.log(error);
+        res.status(500).json({error: "failed to get equipment secondary attributes"});
+    }
+
 }
