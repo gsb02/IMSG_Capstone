1e59c3cc3569b56ea7509ff8f002983a7001c877
Graham Bridges <graham.bridges@maine.edu>
2024-03-05 14:17:07 -0500

    changed css of add Player and mapped teams to database

--- frontend/src/pages/addPlayer.jsx
+++ frontend/src/pages/addPlayer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 
@@ -7,7 +7,8 @@ const AddPlayer = ({ onAddPlayer }) => {
     const handleBackClick = () => {
         navigate(-1);
     }
-
+    const [teams, setTeams] = useState([]);
+    const [isHovering, setIsHovering] = useState(false);
     const [playerName, setPlayerName] = useState('');
     const [age, setAge] = useState('');
     const [sport, setSport] = useState('');
@@ -15,6 +16,19 @@ const AddPlayer = ({ onAddPlayer }) => {
     const [grade, setGrade] = useState('');
     const [isCoach, setIsCoach] = useState("0");
     const [jerseyNum, setJerseyNum] = useState('');
+
+    useEffect(() => {
+        const fetchTeams = async() => {
+            try { 
+                const response = await axios.get('http://localhost:3000/teams');
+                setTeams(response.data);
+            } catch (error) {
+                console.error('There was an error fetching the teams:', error);
+            }
+        };
+
+        fetchTeams();
+    }, []);
 
     // Define styles for input and label grouping
     const formGroupStyle = { 
@@ -27,7 +41,7 @@ const AddPlayer = ({ onAddPlayer }) => {
     };
 
     const inputStyle = { 
-        width: '100%', // Take the full width of the formGroupStyle
+        width: '300px', // Take the full width of the formGroupStyle
         height: '30px',
         marginTop: '5px', // Add some space between label and input
     };
@@ -35,7 +49,8 @@ const AddPlayer = ({ onAddPlayer }) => {
     const formStyle = { 
         display: 'flex', 
         flexDirection: 'column', 
-        alignItems: 'center', 
+        alignItems: 'center',
+        marginTop: '60px', 
         marginBottom: '20px',
     };
 
@@ -44,7 +59,22 @@ const AddPlayer = ({ onAddPlayer }) => {
         flexDirection: 'column',
         alignItems: 'center',
         marginTop: '40px', 
+        position: 'relative',
+        width: '100%'
     };
+
+    const backButtonStyle = {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        backgroundColor: 'transparent',
+        border: 'none',
+        color: isHovering ? '#3498db' : 'inherit',
+        cursor: 'pointer',
+        padding: '10px',
+        transition: 'color o.3s ease',
+        textDecoration: 'underline'
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -59,6 +89,7 @@ const AddPlayer = ({ onAddPlayer }) => {
 
     return (
         <div style= {containerStyle}>
+            <button onClick={handleBackClick} style={backButtonStyle} onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>Back</button>
             <form onSubmit={handleSubmit} style={formStyle}>
                 {/* Wrap each label-input pair in a div with formGroupStyle */}
                 <div style={formGroupStyle}>
@@ -77,12 +108,9 @@ const AddPlayer = ({ onAddPlayer }) => {
                     <label>Team:</label>
                     <select value={teamID} onChange={(e) => setTeamID(e.target.value)} required style={inputStyle}>
                         <option value="" disabled>Select a team</option>
-                        <option value="1">Men's Basketball</option>
-                        <option value="2">Women's Field Hockey</option>
-                        <option value="3">Men's Football</option>
-                        <option value="4">Men's Ice Hockey</option>
-                        <option value="5">Women's Soccer</option>
-                        <option value="6">Women's Softball</option>
+                        {teams.map((team) => (
+                            <option key={team.teamId} value={team.teamId}>{team.teamName}</option>
+                        ))}
                     </select>
                 </div>
                 <div style={formGroupStyle}>
@@ -106,12 +134,10 @@ const AddPlayer = ({ onAddPlayer }) => {
                     <label>Jersey Number:</label>
                     <input type="number" value={jerseyNum} onChange={(e) => setJerseyNum(e.target.value)} required style={inputStyle}/>
                 </div>
-                <button type="submit" style={{ width: '310px', height: '40px', marginTop: '20px' }}>Add Player</button>
+                <button type="submit" style={{ backgroundColor: "#3498db", color: "white", width: '100px', height: '40px', marginTop: '20px', borderRadius: '10px' }}>Add Player</button>
             </form>
-            <button onClick={handleBackClick}>Back</button>
         </div>
     );
 };
 
 export default AddPlayer;
-
